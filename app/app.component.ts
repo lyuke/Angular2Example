95f1c060696e3aa4ebf7713d71e8fada4b01035a
Lyuke <lk11nju@gmail.com>
2016-04-29 22:03:18 +0800

    init

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/main.html',
+    styleUrls:['app/main.css']
+
+
+})
+export class AppComponent { }
